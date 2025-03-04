ba1261247dd1bbc836894be0b7356a81ed0d1770
UMAIR9235 <umairslm80@gmail.com>
2025-03-04 15:34:32 +0530

    remove lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import NewGsap from "@/components/new-gsap";
-import PathDrawing from "@/components/path-drawing";
-import Test from "@/components/test";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-black min-h-screen p-16">
-      {/* <Test /> */}
-      {/* <PathDrawing /> */}
       <NewGsap />
     </div>
   );
